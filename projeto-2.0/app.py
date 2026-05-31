c8ffe4bb4ac9f5ac8e80017edef47abebb526d3b
mb7-alt <mbdissenha17@gmail.com>
2026-05-31 20:43:41 -0300

    Add files via upload

--- projeto-2.0/app.py
+++ projeto-2.0/app.py
@@ -10,5 +10,21 @@ def login():
 def home():
     return render_template('home.html', active_page='Home')
 
+@app.route('/controle-de-itens')
+def cont():
+    return render_template('cont.html', active_page='Controle dos itens')
+
+@app.route('/lista')
+def lista():
+    return render_template('lista.html', titulo="Adicionar à lista")
+
+@app.route('/home-admin')
+def admin():
+    return render_template('admin.html', titulo="Página do administrador")
+
+@app.route('/cadastro-de-usuários')
+def usuarios():
+    return render_template('users.html', titulo="Adicionar novos usuários")
+
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
