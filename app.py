c38892ce37a8ea0d823d7ac0acbffcc6c93af825
mb7-alt <mbdissenha17@gmail.com>
2026-06-12 17:00:58 -0300

    Conexão de login bem-sucedida

--- app.py
+++ app.py
@@ -1,24 +1,49 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 import mysql.connector
 
 app = Flask(__name__)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('index.html', titulo="Página de login")
+    if request.method == 'POST':
+        usuario_digitado = request.form['username']
+        senha_digitada = request.form['password']
+        
+        conn_login = mysql.connector.connect(
+            host='localhost', database='almoxarifado', user='root', password='', port='3306'
+        )
+        cursor = conn_login.cursor(dictionary=True) 
+        cursor.execute("SELECT * FROM usuarios WHERE email = %s", (usuario_digitado,))
+        usuario_encontrado = cursor.fetchone()
+        
+        cursor.close()
+        conn_login.close()
+        if usuario_encontrado and senha_digitada == usuario_encontrado['senha']:
+            if usuario_encontrado['tipo'] == 'admin':
+                return redirect('/home-admin')
+            else:
+                return redirect('/home')
+        else:
+            return "Usuário ou senha incorretos! Tente novamente."
+        
+    return render_template('index.html', titulo="Login")
+            
 
 @app.route('/home')
 def home():
-    db = mysql.connector.connect (
-        host = 'localhost',
-        database = 'almoxarifado',
-        user = 'root',
-        password = '',
-        port = '3307'
+    db = mysql.connector.connect(
+        host='localhost',
+        database='almoxarifado',
+        user='root',
+        password='',
+        port='3306'
     )
     cursor = db.cursor()
     cursor.execute("SELECT * FROM itens")
     resultados = cursor.fetchall()
+    cursor.close()
+    db.close()
+    
     return render_template('home.html', active_page='Home', resultados=resultados)
 
 @app.route('/controle-de-itens')
