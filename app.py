75c643cad2da7547a901df911a42b24691791a0c
mb17-alt <mbdissenha17@gmail.com>
2026-06-24 16:57:07 -0300

    24/06/26 atualização de bcrypt

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 from flask import Flask, render_template, redirect, request
 import mysql.connector
+import bcrypt
 
 app = Flask(__name__)
+
+def db_conexao():
+     conexao = mysql.connector.connect (
+        host = 'localhost',
+        database = 'almoxarifado',
+        user = 'root',
+        password = '',
+        port = '3307'
+    )
+     return conexao
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -9,58 +20,54 @@ def login():
         usuario_digitado = request.form['username']
         senha_digitada = request.form['password']
         
-        conn_login = mysql.connector.connect(
-            host='localhost', database='almoxarifado', user='root', password='', port='3306'
-        )
+        conn_login = db_conexao()
         cursor = conn_login.cursor(dictionary=True) 
         cursor.execute("SELECT * FROM usuarios WHERE email = %s", (usuario_digitado,))
         usuario_encontrado = cursor.fetchone()
         
         cursor.close()
         conn_login.close()
-        if usuario_encontrado and senha_digitada == usuario_encontrado['senha']:
+        
+        if usuario_encontrado and bcrypt.checkpw(senha_digitada.encode('utf-8'), usuario_encontrado['senha'].encode('utf-8')):
             if usuario_encontrado['tipo'] == 'admin':
                 return redirect('/home-admin')
             else:
                 return redirect('/home')
         else:
-            return "Usuário ou senha incorretos! Tente novamente."
+            return redirect('/incorreto')
+                
+    return render_template('index.html')
         
-    return render_template('index.html', titulo="Login")
-            
+@app.route('/incorreto')
+def erroLogin():
+    return render_template('login_incorreto.html')
 
 @app.route('/home')
 def home():
-    db = mysql.connector.connect (
-        host = 'localhost',
-        database = 'almoxarifado',
-        user = 'root',
-        password = '',
-        port = '3307'
-    )
-    cursor = db.cursor()
+    conexao = db_conexao()
+    cursor = conexao.cursor()
     cursor.execute("SELECT * FROM itens")
     resultados = cursor.fetchall()
     cursor.close()
-    db.close()
+    conexao.close()
     
-    return render_template('home.html', active_page='Home', resultados=resultados)
+    return render_template('home.html', resultados=resultados)
 
 @app.route('/controle-de-itens')
 def cont():
-    return render_template('cont.html', active_page='Controle dos itens')
+    return render_template('cont.html')
 
 @app.route('/lista')
 def lista():
-    return render_template('lista.html', titulo="Adicionar à lista")
+    return render_template('lista.html')
 
 @app.route('/home-admin')
 def admin():
-    return render_template('admin.html', titulo="Página do administrador")
+    return render_template('admin.html')
 
 @app.route('/cadastro-de-usuarios')
-def usuarios():
-    return render_template('users.html', titulo="Adicionar novos usuários")
+def users():
+    return render_template('users.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
