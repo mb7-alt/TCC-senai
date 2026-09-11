cdd3f03eee83a88cd16bf38e1c1ff5dc796d0e1a
Aluno <senai@senai.com>
2026-09-11 16:33:55 -0300

    Iniciando a criação de outras páginas do mobile

---
+++ mobile/Telas/Home.js
@@ -0,0 +1,35 @@
+export function Home() { 
+  const [produtos, setProdutos] = useState([]);
+
+  const [id, setId] = useState('');
+  const [nome, setNome] = useState('');
+  const [preço, setPreço] = useState('');
+  const [quantidade, setQuantidade] = useState('');
+  const [min, setMin] = useState('');
+  const [categoria, setCategoria] = useState('');
+  const [descriçao, setDescriçao] = useState('');
+  const [imagem, setImagem] = useState('');
+
+
+  const pegarTabela = async () => {
+    try{
+        const resposta = await fetch('http://10.154.20.83:5000/api/itens', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+    }
+
+    finally
+    {};
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text>
+        {dados}
+      </Text>
+    </View>
+  );
+}
---
+++ mobile/Telas/Login.js
@@ -0,0 +1,31 @@
+export function entrar() {
+  const [email, setItem] = useState('');
+  const [senha, setQtde] = useState('');
+
+  const enviarFormulario = async () => {
+    await fetch('http://10.154.20.83:5000/api/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, senha }),
+    });
+  };
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        placeholder="Digite o email"
+        value={email}
+        onChangeText={setItem}
+      />
+      <TextInput
+        placeholder="Digite a senha"
+        value={senha}
+        onChangeText={setQtde}
+        keyboardType="numeric"
+      />
+      <Button title="Enviar Dados" onPress={enviarFormulario} />
+    </View>
+  );
+}
